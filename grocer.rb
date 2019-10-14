35ac31dddddb39195d3c9401c25c6d0b61bea592
Samir Jasarat <samirjasarat@gmail.com>
2019-10-14 17:25:52 -0400

    Done.

--- grocer.rb
+++ grocer.rb
@@ -44,9 +44,32 @@ end
 
 
 def apply_clearance(cart)
-  # code here
+  #The item and price_hash keys are iterate over in the following
+  #hash.
+  cart.each do |item, price_hash|
+    #The item is checked to see if it is infact on clearance. If it is, 
+    #Then, the clearance discount is applied to the item.
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash [:price] * 0.8).round(2)
+end
+end
+cart
 end
 
-def checkout(cart, coupons)
-  # code here
+#The checkout method is initially defined used the cart and coupon hashes
+#As argumemts
+
+ef checkout(items, coupons)
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+  
+  total = 0
+  
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
+  end
+  
+  total > 100 ? total * 0.9 : total
+  
 end
